0d2717251147c99331c6472f108452bf52210de6
Mike Mariano <mike@uiuxartist.com>
2017-01-10 22:15:15 -0800

    Initial Commit

---
+++ ExportArtboardsAsSVG.sketchplugin/Contents/Sketch/ExportArtboardsAsSVG.js
@@ -0,0 +1,72 @@
+@import 'common.js'
+
+var onRun = function(context) {
+	//reference the sketch document
+	var doc = context.document;
+	//reference the pages array in the document
+	var pages = [doc pages];
+
+	var file_path = selectFolder();
+
+	//export all the pngs to the folder
+	exportAllSymbols(doc, pages, file_path);
+
+	//if user doesn't press cancel, alert that the export is done
+	if(file_path != undefined){
+		alert("Symbols Exported!", "Symbols exported to : "+file_path);
+	}
+}
+
+function selectFolder(){
+  //open a window to select a folder to save to
+  var panel = [NSOpenPanel openPanel];
+  [panel setCanChooseDirectories:true];
+  [panel setCanCreateDirectories:true];
+
+  //checks if user clicks open in window
+  var clicked = [panel runModal];
+  if (clicked == NSFileHandlingPanelOKButton) {
+
+    var isDirectory = true;
+    var firstURL = [[panel URLs] objectAtIndex:0];
+    var unformattedURL = [NSString stringWithFormat:@"%@", firstURL];
+
+    //makes sure spaces aren't formatted to %20
+    var file_path = [unformattedURL stringByRemovingPercentEncoding];
+
+    //removes file:// from path
+    if (0 === file_path.indexOf("file://")) {
+       file_path = file_path.substring(7);
+       return file_path;
+    }
+  }
+}
+
+function exportAllSymbols(doc, pages, file_path){
+  //loop through the pages array
+	for (var i = 0; i < pages.count(); i++){
+
+		//reference each page
+		var page = pages[i];
+
+    //get the page name
+    var pageName = [page name];
+
+		var artboards = [page artboards];
+
+		log(artboards.count());
+
+		for (var z = 0; z < artboards.count(); z++){
+
+			//reference each artboard of each page
+			var artboard = artboards[z];
+
+			//get the name of the artboard and uses it as the file name
+			var artboardName = [artboard name];
+
+			//export the artboard
+			doc.saveArtboardOrSlice_toFile(artboard,file_path+"/"+artboardName+".svg");
+
+		}
+	}
+}
